d041ddf2ffc0dec1c3b8519910b95d43a157e6f8
Ruslan <111281649+3iN2@users.noreply.github.com>
2023-03-25 18:37:18 +0300

    Домашнее задание 4
    Массивы и матрицы

---
+++ HomeWork4/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите количество строк матрицы");
+            int rowMatrix = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите количество столбцов матрицы");
+            int colMatrix = int.Parse(Console.ReadLine());
+
+            int[,] a = new int[rowMatrix,colMatrix];
+            Random randomNumber = new Random();
+
+            //Задание 1
+            Console.WriteLine("Полученная матрица А: ");
+            int sum = 0;
+            for (int i = 0; i < rowMatrix; i++)
+            {
+                for (int j = 0; j < colMatrix; j++)
+                {
+                    a[i, j] = randomNumber.Next(1,10);
+                    sum += a[i, j];
+                    Console.Write($" {a[i, j]} ");
+                }
+                Console.WriteLine(); //Для читабельности матрицы в консоли
+            }
+            Console.WriteLine($"Сумма чисел в матрице {sum}");
+            Console.WriteLine();
+
+            //Задание 2
+            int[,] b = new int[rowMatrix, colMatrix];
+
+            Console.WriteLine("Полученная матрица B: ");
+            for (int i = 0; i < rowMatrix; i++)
+            {
+                for (int j = 0; j < colMatrix; j++)
+                {
+                    b[i, j] = randomNumber.Next(1, 10);
+                    sum += b[i, j];
+                    Console.Write($" {b[i, j]} ");
+                }
+                Console.WriteLine(); //Для читабельности матрицы в консоли
+            }
+            Console.WriteLine("Сумма матриц A и B: ");
+
+            int[,] c = new int [rowMatrix, colMatrix];
+
+            for (int i = 0; i < rowMatrix; i++)
+            {
+                for (int j = 0; j < colMatrix; j++)
+                {
+                    c[i, j] = a[i, j] + b[i, j];
+                    Console.WriteLine($" {c[i, j]} ");
+                }
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
---
+++ Work 3 task 2/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Добрый день игрок. Сколько у вас на руках карт? ");
+            int numberСards = int.Parse(Console.ReadLine());
+            int sum = 0;
+            int Number = 0;
+            Console.WriteLine("Введите наминал каждой карты.\nДля карт наминалом выше 10 используйте следующие обозначения:\n Валет - J\n Дама - Q\n Король - K\n Туз - T");
+
+            for (int i = 1; i <= numberСards; i++)
+            {
+                string priceCard = (Console.ReadLine());
+                switch (priceCard)
+                {
+                    case "T":
+                        {
+                            sum += 10;
+                            continue;
+                        }
+                    case "J":
+                        {
+                            sum += 10;
+                            continue;
+                        }
+                    case "Q":
+                        {
+                            sum += 10;
+                            continue;
+                        }
+                    case "K":
+                        {
+                            sum += 10;
+                            continue;
+                        }
+                }
+                Number = int.Parse(priceCard);
+                sum = sum + Number;
+                continue;
+
+            }
+            Console.WriteLine($"Суммарный наминал ваших карт  = {sum}");
+            Console.ReadKey();
+        }
+    }
+}
